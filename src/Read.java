7c6141e7bb9569526737981153f2d72e3b74826d
haraguchi <syotabgp@gmail.com>
2016-02-16 18:27:00 +0900

    commit

--- src/Read.java
+++ src/Read.java
@@ -2,77 +2,108 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class Read {
 	public static void main(String[] args){
-		ArrayList<String> bank = new ArrayList<String>();
-		ArrayList<String> toy  = new ArrayList<String>();
-		Map<String,String> map  = new HashMap<String, String>();
-		List<String> list = new ArrayList<String>();
+		HashMap<String,String> map = new HashMap<String,String>();
+		HashMap<String,String> maps = new HashMap<String,String>();
+		HashMap<String,String> list = new HashMap<String,String>();
+		HashMap<String,String> lists = new HashMap<String,String>();
+
 
 		try{
 			File file = new File(args[0],"branch.lst");
-			File files = new File(args[0],"commodity.lst");
-
 			if(!file.exists()){
 				System.out.println("支店定義ファイルがありません");
-			//}else if(file.getPath().endsWith(".lst")){
-				System.out.println("支店定義ファイルのフォーマットが不正です");
-			//  return;
-
-			if(!files.exists()){
-				System.out.println("商品定義ファイルがありません");
-			//}else if(file.getPath().endsWith(".lst")){
-				System.out.println("商品定義ファイルのフォーマットが不正です");
 				return;
-			}
 			}
 
 			BufferedReader br = new BufferedReader(new FileReader(file));
-			BufferedReader brs = new BufferedReader(new FileReader(files));
-			String a;
 			String s;
 
-			while((s = br.readLine())!= null) {
-				bank.add(s);
+			while((s = br.readLine()) != null){
+				String array[] = s.split(",");
+				//System.out.println(array[0]);
+				if(array.length != 2) throw new NumberFormatException();
+				String cd   = (array[0]);
+				String name = (array[1]);
+				map.put(cd, name);
+				//System.out.println(map.get("001"));
 			}
 			br.close();
 
-			while((a = brs.readLine())!= null){
-				toy.add(a);
+		}catch(IOException e){
+			System.out.println(e);
+		}catch(NumberFormatException e){
+			System.out.println("支店定義ファイルのフォーマットは不正です");
+			return;
+		}
+
+		try{
+			File files = new File(args[0],"commodity.lst");
+			if(!files.exists()){
+				System.out.println("商品定義ファイルがありません");
+				return;
+			}
+
+			BufferedReader brs = new BufferedReader(new FileReader(files));
+			String a;
+			while((a = brs.readLine()) != null){
+				String array[] = a.split(",");
+				if(array.length != 2) throw new NumberFormatException();
+				String shcord = array[0];
+				String shname = array[1];
+				maps.put(shcord,shname);
 			}
 			brs.close();
 
+		}catch(IOException a){
+			System.out.println(a);
+		}catch(NumberFormatException a){
+			System.out.println("支店定義ファイルのフォーマットは不正です");
+			return;
+		}
+
+		try{
 			File dir = new File(args[0]);
 			File[] f1 = dir.listFiles();
 			for(int i = 0; i < f1.length; i++){
 				if(f1[i].getName().endsWith(".rcd")){
 					File f2 = f1[i];
 
-			BufferedReader bb = new BufferedReader(new FileReader(f2));
+			BufferedReader  bb = new BufferedReader(new FileReader(f2));
 			String b;
 			while((b = bb.readLine())!= null){
-				System.out.println(b);
-				list.add(b);
-				String sell = list.get(0);
-
+				String array[] = b.split(",");
+				if(array.length != 3) throw new NumberFormatException();
+				String cd = array[0];
+				String shcord = array[1];
+				String sell = array[2];
+				list.put(cd, sell);
+				lists.put(shcord, sell);
+				System.out.println("001");
 			}
 			bb.close();
-
 				}
 			}
-//			System.out.println(list.get(2));
-//			System.out.println(list.get(5));
-//			System.out.println(list.get(8));
-
-		}catch(IOException e){
-			System.out.println(e);
+		}catch(IOException x){
+			System.out.println(x);
+		}catch(NumberFormatException x){
+			System.out.println("該当ファイル名のフォーマットは不正です");
+			return;
 		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
 
 
